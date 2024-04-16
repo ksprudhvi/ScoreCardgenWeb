26b3d0337f50e20b11e9929a590283251b9cc202
ksprudhvi <91111944+ksprudhvi@users.noreply.github.com>
2024-04-16 09:57:08 +0000

    home Screen

--- scorecardgen/src/app/app.routes.ts
+++ scorecardgen/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomescreenComponent } from './homescreen/homescreen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomescreenComponent },
+];
