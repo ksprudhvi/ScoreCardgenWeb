a36fd0f33fd03aa10f7b4e44c238a92c6da40e09
ksprudhvi <91111944+ksprudhvi@users.noreply.github.com>
2024-04-17 06:14:57 +0000

    uploader

--- scorecardgen/src/app/app.routes.ts
+++ scorecardgen/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomescreenComponent } from './homescreen/homescreen.component';
 import { CompetationDetailsComponent } from './competation-details/competation-details.component';
 import { CreateCompetitionComponent } from './create-competition/create-competition.component';
+import { FileUploadService } from './services/file-upload.service';
 
 
 
@@ -10,5 +11,5 @@ export const routes: Routes = [
     { path: '', component: HomescreenComponent },
     { path: 'details', component: CompetationDetailsComponent },
     { path: 'add', component: CreateCompetitionComponent },
-
+    { path: 'upload', component: FileUploadService },
 ];
